a21e6d706811a7b9acd0a803dc4d5e824646b916
tech master <bryan77@twc.com>
2021-05-01 08:03:03 -0400

    add default props and create DogList component for home route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import React, {Component} from 'react';
+import DogList from './DogList';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
+import gadget from './img/gadget.jpg';
+import dino from './img/dino.jpg';
+import tubby from './img/tubby.jpg';
 
 class App extends Component {
+  static defaultProps = {
+    dogs: [
+      {
+        name: "Gadget",
+        age: 5,
+        src: gadget,
+        facts: [
+          "Gadget loves eating popcorn.",
+          "Gadget is a terrible guard dog.",
+          "Gadget wants to cuddle with you."
+        ]
+      },
+      {
+        name: "Dino",
+        age: 3,
+        src: dino,
+        facts: [
+          "Dino has sooo much energy.",
+          "Dino is highly intelligent.",
+          "Dino loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tubby",
+        age: 8,
+        src: tubby,
+        facts: [
+          "Tubby is not the brightest dog.",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food."
+        ]
+      }
+    ]
+  };
+
   render() {
-    return (
-      <div>
-        <h1 className="display-2">My Fun App!</h1>
-      </div>
-    );
+    return <Route path="/home" render={() => <DogList dogs={this.props.dogs} />} />;
   }
 }
 
