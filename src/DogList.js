0cdb834dd45500a476129757f18eeb401be4a9d9
tech master <bryan77@twc.com>
2021-05-06 07:16:02 -0400

    add content to home route and some styles

--- src/DogList.js
+++ src/DogList.js
@@ -1,13 +1,36 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
 
-class DogList extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Dog List!</h1>
-            </div>
-        )
-    }
+// class DogList extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Dog List!</h1>
+//             </div>
+//         )
+//     }
+// }
+
+// export default DogList;
+
+import React, { useState} from 'react';
+import './DogList.css';
+
+function DogList(props) {
+    return (
+        <div className="DogList">
+            <h1 className="display-1 text-center">Dog List!</h1>
+            <div className="container">
+                <div className="row">
+                    {props.dogs.map(d => (
+                        <div className="dog col-md-4 text-center" key={d.name}>
+                            <img src={d.src} alt={d.name} />
+                            <h3>{d.name}</h3>
+                        </div>
+                    ))}                
+                </div>
+            </div>                
+        </div>        
+    )
 }
 
 export default DogList;
