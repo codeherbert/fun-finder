b189ba8343badf35457174c44032f1ad7432ec54
tech master <bryan77@twc.com>
2021-05-20 08:07:32 -0400

    add details component for additional details route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import DogList from './DogList';
+import DogDetails from './DogDetails';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import gadget from './img/gadget.jpg';
@@ -43,7 +44,19 @@ class App extends Component {
   };
 
   render() {
-    return <Route path="/home" render={() => <DogList dogs={this.props.dogs} />} />;
+    const getDog = props => {
+      let name = props.match.params.name;
+      let currentDog = this.props.dogs.find(
+        dog => dog.name.toLowerCase() === name.toLowerCase()
+      );
+      return <DogDetails {...props} dog={currentDog} />
+    }
+    return (
+      <Switch>
+        <Route exact path="/home" render={() => <DogList dogs={this.props.dogs} />} />;
+        <Route exact path="/home/:name" render={getDog} />;
+      </Switch>
+    );
   }
 }
 
